ea4d4ac631ce097d3f409d6b6c6a48c59ac301a3
Smith, Tim <Tim.Smith@inin.com>
2016-12-06 16:04:10 -0700

    API-2055 Adding request duration tracking

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 var purecloud = require('purecloud_api_sdk_javascript');
 var Q = require('q');
+var moment = require('moment');
 
 var config = require('./config');
 var Logger = require('./logger');
@@ -30,8 +31,11 @@ function Api() {
 Api.prototype.login = function() {
 	var deferred = Q.defer();
 
+	var startTime = new moment();
 	this.pureCloudSession.login()
 		.then(function() {
+			if (config.args.debugapi === true) 
+				log.verbose('Request "login" completed in ' + moment().diff(startTime, new moment()) + ' ms');
 			return deferred.resolve();
 		})
 		.catch(function(error) {
@@ -45,8 +49,11 @@ Api.prototype.postConversationsDetailsQuery = function(query) {
 	var deferred = Q.defer();
 	var body = {};
 
+	var startTime = new moment();
 	this.analyticsApi.postConversationsDetailsQuery(query)
 		.then(function(result){
+			if (config.args.debugapi === true) 
+				log.verbose('Request "postConversationsDetailsQuery" completed in ' + moment().diff(startTime, new moment()) + ' ms');
 			deferred.resolve(result);
 		})
 		.catch(function(error){
@@ -60,8 +67,11 @@ Api.prototype.postConversationsAggregatesQuery = function(query) {
 	var deferred = Q.defer();
 	var body = {};
 
+	var startTime = new moment();
 	this.analyticsApi.postConversationsAggregatesQuery(query)
 		.then(function(result){
+			if (config.args.debugapi === true) 
+				log.verbose('Request "postConversationsAggregatesQuery" completed in ' + moment().diff(startTime, new moment()) + ' ms');
 			deferred.resolve(result);
 		})
 		.catch(function(error){
@@ -75,8 +85,11 @@ Api.prototype.getPermissions = function() {
 	var deferred = Q.defer();
 	var body = {};
 
+	var startTime = new moment();
 	this.authorizationApi.getPermissions()
 		.then(function(result){
+			if (config.args.debugapi === true) 
+				log.verbose('Request "getPermissions" completed in ' + moment().diff(startTime, new moment()) + ' ms');
 			deferred.resolve(result);
 		})
 		.catch(function(error){
