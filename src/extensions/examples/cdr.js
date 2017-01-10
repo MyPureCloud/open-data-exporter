36b37d8b8485f0a71db80cea9cad586025a2819f
Smith, Tim <Tim.Smith@inin.com>
2017-01-10 07:29:25 -0700

    API-2158 adding participant data

--- src/extensions/examples/cdr.js
+++ src/extensions/examples/cdr.js
@@ -23,6 +23,50 @@ CDR.prototype.countArray = function(arr) {
 	return arr.length;
 };
 
+CDR.prototype.coalesceConversationIds = function(data) {
+	var conversationIds = [];
+	_.forEach(data.conversations, function(conversation) {
+		conversationIds.push(conversation.conversationId);
+	});
+	return conversationIds;
+};
+
+CDR.prototype.aggregateParticipantAttributes = function(data) {
+	// Iterate conversations
+	_.forEach(data.cdr_request.conversations, function(conversation) {
+		// Get conversation w/participant data
+		var c2 = data.cdr_conversation_details_request[conversation.conversationId];
+
+		// Iterate participants
+		_.forEach(conversation.participants, function(participant) {
+			// Find participant and set data
+			_.forEach(c2.participants, function(p2) {
+				// Note: the analytics and conversation models have slight differences. 
+				// One example is a participant's GUID being stored as "id" vs. "participantId"
+				if (participant.participantId == p2.id) {
+					participant.attributes = p2.attributes;
+
+					// Stop this loop
+					return false;
+				}
+			});
+		});
+	});
+};
+
+CDR.prototype.ensureAttributes = function(data) {
+	_.forEach(data.conversations, function(conversation) {
+		_.forEach(conversation.participants, function(participant) {
+			if (!participant.attributes)
+				participant.attributes = {};
+
+			// Ensure certain attributes are initialized
+			if (!participant.attributes.accountNumber)
+				participant.attributes.accountNumber = '';
+		});
+	});
+};
+
 
 
 module.exports = new CDR();
